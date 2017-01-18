7182ae9c4a580cc52ce56d8a1f62750276320d68
subbu <subbu1125@gmail.com>
2017-01-17 22:23:15 -0800

    redux

---
+++ client/component/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PhotoGrid = React.createClass({
+	render() {
+		return (
+			<div className="photo-grid">
+				Hi Here Photo grid Page
+			</div>
+		)
+	}
+});
+
+export default PhotoGrid;
---
+++ client/component/Single.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Single = React.createClass({
+	render() {
+		return (
+			<div className="single-photo">
+        Hi Here single Photo Page
+			</div>
+		)
+	}
+});
+
+export default Single;
