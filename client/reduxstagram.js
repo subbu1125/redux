8bc2f930fce163a4501a16c1638e695725dce13e
subbu <subbu1125@gmail.com>
2017-01-18 15:03:23 -0800

    added code

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -2,17 +2,21 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import style from './styles/style.styl';
-import Main from './component/Main';
+import App from './component/App';
 import Single from './component/Single';
 import PhotoGrid from './component/PhotoGrid';
+import { Provider } from 'react-redux';
+import store, { history } from './store';
 
 const router = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}>
-      <IndexRoute component={PhotoGrid}></IndexRoute>
-      <Route path="/view/:postId" component={Single}></Route>
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path="/view/:postId" component={Single}></Route>
+      </Route>
+    </Router>
+  </Provider>
 )
 
 
